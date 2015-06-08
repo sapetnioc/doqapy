71ced9ca8adb873cadcd8a2c202fddf60d9fd93a
sapetnioc <sapetnioc@users.noreply.github.com>
2015-06-08 18:13:20 +0200

    Make cati to document independent from doqapy

--- wip/cati_to_doqapy.py
+++ wip/cati_to_doqapy.py
@@ -6,84 +6,121 @@ import re
 import uuid
 
 import catidb
-import doqapy
 
-def new_subjects(db, action, study):
-    split = re.compile(r'(\d+)_?([A-Z]+)')
-    for code in action['subjects_code']:
-        document = {
-            '_ref': '%s/subject/' % study,
-            'code': code,
-            'study': studies[study],
-        }
-        
-        match = split.match(code)
-        if match:
-            document['numerical_code'] = match.group(1)
-            document['initials'] = match.group(2)
-        yield document
-
-def process_action_file(db, actions_file, unknown_actions, study):
-    try:
-        actions = json.load(open(actions_file))
-    except ValueError, e:
-        raise ValueError('%s: %s' % (actions_file, str(e)))
-    for action in actions['actions']:
-        action_name = action.pop('name')
-        action.pop('version', None)
-        manager = globals().get(action_name)
-        if manager is None:
-            unknown_actions.add(action_name)
+class CatiSharedToDocuments(object):
+    def __init__(self, studies=None):
+        if studies:
+            studies = set(studies)
+        self.catiflow  = catidb.CatiFlow()
+        cati_shared = self.catiflow.cati_shared
+        self.studies = {}
+        for study in os.listdir(cati_shared):
+            if studies and study.lower() not in studies:
+                continue
+            study_controller = self.catiflow.find_study_controller(study)
+            if study_controller is not None:
+                self.studies[study.lower()] = study_controller
+    
+    def is_selected_study(self, study):
+        if self.studies:
+            return stydy.lower() in seld.studies
+        return True
+    
+    _new_subjects_split = re.compile(r'(\d+)_?([A-Z]+)')
+    def new_subjects(self, action, study, documents_to_yield):
+        for code in action['subjects_code']:
+            document = {
+                '_id': code,
+                'study': study.lower(),
+            }
+            
+            match = self._new_subjects_split.match(code)
+            if match:
+                document['numerical_code'] = match.group(1)
+                document['initials'] = match.group(2)
+            yield (study.lower(), 'subjects', document)
+    
+    def subject_group(self, action, study, documents_to_yield):
+        group = action['group']
+        document = documents_to_yield.setdefault(study.lower(),{}).setdefault(('subject_groups',group),{})
+        if document:
+            document['subjects'].extend(action['subject_codes'])
         else:
-            for document in manager(db, action, study):
-                print db.store_document(document)
+            document.update(dict(
+            _id = group,
+            study = study.lower(),
+            subjects = action['subject_codes']))
+        # Turn this function to an empty generator
+        return
+        yield
 
-catiflow  =catidb.CatiFlow()
-cati_shared = catiflow.cati_shared
-db_file = osp.expanduser('~/brainvisa/src/doqapy/cati_shared.doqapy')
-if osp.exists(db_file):
-    os.remove(db_file)
-db = doqapy.connect('sqlite:%s' % db_file)
-
-studies = {}
-db.create_collection('study')
-db.create_field('study.name', 'unicode', create_index=True)
-for study in os.listdir(cati_shared):
-    study_controller = catiflow.find_study_controller(study)
-    if study_controller is not None:
-        study_name = study_controller.name
-        document = dict(
-            _ref = 'study/',
-            name = study_name,
-            main_investigators = [i.strip() for i in study_controller.main_investigator.split(',')],
-            description = study_controller.description,
-            modality = [i.strip() for i in study_controller.modality.split(',')],
-            diseases = [i.strip() for i in study_controller.disease.split(',')],
-        )
-        studies[study_name] = db.store_document(document)
-        subject_collection = '%s/subject' % study_name
-        db.create_collection(subject_collection)
-        db.create_field('%s.study' % subject_collection, 'ref', create_index=True)
-             
-unknown_actions = set()
-for base, directories, files in os.walk(osp.join(osp.dirname(catidb.__file__),'studies')):
-    for f in files:
-        if f.endswith('.json'):
-            study = osp.basename(osp.dirname(base))
-            process_action_file(db, osp.join(base,f), unknown_actions, study=study)
-db.commit()
-
-count = 0
-for study in os.listdir(cati_shared):
-    actions_dir = osp.join(cati_shared, study, 'ACTIONS')
-    if osp.exists(actions_dir):
-        for base, directories, files in os.walk(actions_dir):
+    def actions_file_to_documents(self, actions_file, unknown_actions, study, documents_to_yield):
+        try:
+            actions = json.load(open(actions_file))
+        except ValueError as e:
+            raise ValueError('%s: %s' % (actions_file, str(e)))
+        for action in actions['actions']:
+            action_name = action.pop('name')
+            action.pop('version', None)
+            manager = getattr(self, action_name, None)
+            if manager is None:
+                unknown_actions.add(action_name)
+            else:
+                for dcd in manager(action, study, documents_to_yield):
+                    yield dcd
+    
+    def cati_shared_to_documents(self):
+        studies = {}
+        documents_to_yield = {}
+        for study, study_controller in self.studies.iteritems():
+            study_name = study_controller.name
+            document = dict(
+                _id = study.lower(),
+                name = study_name,
+                main_investigators = [i.strip() for i in study_controller.main_investigator.split(',')],
+                description = study_controller.description,
+                modality = [i.strip() for i in study_controller.modality.split(',')],
+                diseases = [i.strip() for i in study_controller.disease.split(',')],
+            )
+            yield (study.lower(), 'study', document)
+                    
+        unknown_actions = set()
+        for base, directories, files in os.walk(osp.join(osp.dirname(catidb.__file__),'studies')):
             for f in files:
                 if f.endswith('.json'):
-                    count += 1
-                    if count % 100 == 0:
-                        db.commit()
-                    process_action_file(db, osp.join(base,f), unknown_actions, study=study)
+                    study = osp.basename(osp.dirname(base))
+                    for dcd in self.actions_file_to_documents(osp.join(base,f), unknown_actions, study, documents_to_yield):
+                        yield dcd
 
-if unknown_actions:
-    print >> sys.stderr, 'Unknown actions:', ', '.join(sorted(unknown_actions))
+        for study in self.studies():
+            actions_dir = osp.join(self.catiflow.cati_shared, study, 'ACTIONS')
+            if osp.exists(actions_dir):
+                for base, directories, files in os.walk(actions_dir):
+                    for f in files:
+                        if f.endswith('.json'):
+                            for dcd in self.actions_file_to_documents(osp.join(base,f), unknown_actions, study, documents_to_yield):
+                                yield dcd
+            study_documents_to_yield = documents_to_yield.get(study.lower(),{})
+            while study_documents_to_yield:
+                collection_id, document = study_documents_to_yield.popitem()
+                collection, _id = collection_id
+                yield (study.lower(), collection, document)
+
+        if unknown_actions:
+            print >> sys.stderr, 'Unknown actions:', ', '.join(sorted(unknown_actions))
+
+
+if __name__ == '__main__':
+    from pymongo import MongoClient
+    
+    mongo = MongoClient()
+    cleared_db = set()
+    cstd = CatiSharedToDocuments(studies=['mapt'])
+    for database, collection, document in cstd.cati_shared_to_documents():
+        print repr(database)
+        if database not in cleared_db:
+            mongo.drop_database(database)
+            cleared_db.add(database)
+        db = mongo.get_database(database)
+        r = db.get_collection(collection).insert_one(document)
+        print '%s/%s -> %s' % (database, collection, r.inserted_id)
